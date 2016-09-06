904973f6ecd988f2ca67a270cf5c9396643cd786
AadhyaMittal <ammittalaadhya@gmail.com>
2016-09-06 10:04:04 +0530

    Update inversioncount.c

--- inversioncount.c
+++ inversioncount.c
@@ -1,9 +1,13 @@
+//The INVERSION COUNT of a Bubble Sort Algorithm 
+
 #include<stdio.h>
 int main()
 {
     int n, i, j, inv=0, temp;
+    printf("Enter the number of elements ");
     scanf("%d",&n);
     int arr[n];
+    printf("Enter the elements of the array ");
     for(i = 0; i < n; i++)
     {
         scanf("%d",&arr[i]);
@@ -21,5 +25,5 @@ int main()
             }
         }
     }
-    printf("%d",inv);
+    printf("The inversion count is %d",inv);
 }
