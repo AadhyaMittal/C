602d7b9cd271c4b653c0470cb0b910b90d421217
AadhyaMittal <ammittalaadhya@gmail.com>
2016-09-06 10:07:47 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+print_square(int n, int m)
+{
+    int i, sq;
+    sq = n*n;
+    for(i = 0; i < m; i++)
+    {
+        printf("%d ",sq);
+        sq = sq + ((n<<1) + 1);
+        n++;
+    }
+}
+int main()
+{
+    int n, m;
+    scanf("%d",&n);
+    scanf("%d",&m);
+    print_square(n,m);
+}
